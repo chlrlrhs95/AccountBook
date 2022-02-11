514bd6c8093a0415e4d62ced762673a35ff1f931
CubeDr <jbhyunikim@gmail.com>
2022-02-11 15:02:01 +0900

    Add Layout for DashboardFragment

---
+++ app/src/main/java/com/example/accountbook/DashboardFragment.kt
@@ -0,0 +1,18 @@
+package com.example.accountbook
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DashboardFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+    companion object {
+        fun newInstance() = DashboardFragment()
+    }
+}
